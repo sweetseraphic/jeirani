0cb69625e3f39ed6546869f1bde888ccf7f6d8bf
sweetseraphic <84316989+sweetseraphic@users.noreply.github.com>
2021-05-30 20:11:02 +0400

    Update ჯეირანი.js

--- ჯეირანი.js
+++ ჯეირანი.js
@@ -1,3 +1,33 @@
+// 1 შექმენით ცვლადი სახელწოდებით myVariable და მიანიჭეთ მას მნიშვნელობა 5. 
+var myVariable = 5;
+console.log(myVariable);
+//2.	შექმენით მეორე ცვლადი სახელწოდებით testVariable და მიანიჭეთ მას ცვლადი myVariable როგორც მნიშვნელობა. 
+var testVariable = myVariable;
+console.log(testVariable);
+//3.	შემოიტანეთ ორი ცვლადი x და y და მიანიჭეთ მას მნიშვნელობები 4 და 5. 
+var x = 4;
+var y = 5;
+//4.	შემოიტანეთ ცვლადი სახელწოდებით sum და მიანიჭეთ მას x და y-ის ჯამი მნიშვნელობად. 
+var sum = x + y;
+console.log(sum);
+//5.	შემოიტანეთ ცვლადი სახელწოდებით product და მიანიჭეთ მას x და y-ის ნამრავლი მნიშვნელობად. 
+var product = x * y;
+console.log(product);
+//6.	შემოიტანეთ ცვლადი სახელწოდებით quotient და მიანიჭეთ მას x და y-ის განაყოფი მნიშვნელობად. 
+var quotient = x / y;
+console.log(quotient);
+//7.	შემოიტანეთ ცვლადი სახელწოდებით difference და მიანიჭეთ მას x და y-ის სხვაობა მნიშვნელობად. 
+var difference = x - y;
+console.log(difference);
+//8 .	გარდაქმენით მოცემულობა += გამოყენებით x = x + 12.
+x += 12;
+console.log(x);
+//9.	შექმენით ახალი ცვლადი სახელწოდებით num და შეინახეთ მასში let str = “66”; მნიშვნელობა, გადაკონვერტირებული number ტიპად. 
+var num = "66";
+console.log(parseInt(num));
+//10.	\n \t \\ გამოყენებით ქვევით მოცემული output-ის მისაღებად შემოიტანეთ ცვლადი სახელწოდებით myStr და მნიშვნელობაში გაუწერეთ შესაბამისი კოდი, არ გამოიყენოთ სფეისი. 
+let myStr = "firsrstLine\nSecondLine \n \t ThirdLine";
+console.log(myStr);
 //ჯეირანი
 let rendom = Math.floor(Math.random() * 3);
 let user = prompt(`შეიყვანეთ სიტყვა: "ქვა" "მაკრატელი" "ქაღალდი"  `)
